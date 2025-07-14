d113f98a4c2396d8e05c91083929ea31655b7ad7
Marce <101135405+Marce952@users.noreply.github.com>
2025-07-14 13:47:20 -0300

    Proveedor frontend

---
+++ src/app/Components/Supplier.jsx
@@ -0,0 +1,70 @@
+'use client'
+import { Button, Input, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, useDisclosure } from '@heroui/react'
+import { MdProductionQuantityLimits } from "react-icons/md";
+import React from 'react'
+import ModalComponent from './ModalComponentAdd';
+
+const Supplier = () => {
+   const {isOpen, onOpen, onOpenChange} = useDisclosure();
+
+  return (
+    <div
+      className='flex flex-col gap-4 p-4'
+    >
+      <ModalComponent 
+        isOpen={isOpen}
+        onOpen={onOpen}
+        onOpenChange={onOpenChange}
+      />
+      {/* Filtros */}
+      <div className='flex justify-around items-center gap-2'>
+        <Input
+          placeholder='Buscar proveedor'
+        />
+        <Button
+          startContent={<MdProductionQuantityLimits fontSize={'200px'} />}
+          variant='solid'
+          color='primary'
+          onPress={onOpen}
+        >
+          Añadir proveedor
+        </Button>
+      </div>
+
+      {/* Ver los productos */}
+      <div>
+        <Table aria-label="Example static collection table">
+          <TableHeader>
+            <TableColumn>NAME</TableColumn>
+            <TableColumn>ROLE</TableColumn>
+            <TableColumn>STATUS</TableColumn>
+          </TableHeader>
+          <TableBody>
+            <TableRow key="1">
+              <TableCell>Tony Reichert</TableCell>
+              <TableCell>CEO</TableCell>
+              <TableCell>Active</TableCell>
+            </TableRow>
+            <TableRow key="2">
+              <TableCell>Zoey Lang</TableCell>
+              <TableCell>Technical Lead</TableCell>
+              <TableCell>Paused</TableCell>
+            </TableRow>
+            <TableRow key="3">
+              <TableCell>Jane Fisher</TableCell>
+              <TableCell>Senior Developer</TableCell>
+              <TableCell>Active</TableCell>
+            </TableRow>
+            <TableRow key="4">
+              <TableCell>William Howard</TableCell>
+              <TableCell>Community Manager</TableCell>
+              <TableCell>Vacation</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  )
+}
+
+export default Supplier
---
+++ src/app/pages/proveedores/page.jsx
@@ -0,0 +1,10 @@
+import Supplier from '../../Components/Supplier'
+import React from 'react'
+
+const page = () => {
+  return (
+    <Supplier />
+  )
+}
+
+export default page
